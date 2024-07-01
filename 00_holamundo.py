d9e88895b131ca7836bdeb9d252491ddff4f9eb0
RodrigoAhumadaE <rod.ahumada@duocuc.cl>
2024-07-01 19:33:36 -0400

    Primera clase 01072024

---
+++ 00_holamundo.py
@@ -0,0 +1,44 @@
+# Esto es un comentario (ctrl+ç o altGr+3)
+
+'''
+esto es un comentario
+en varias
+lineas
+'''
+
+"""
+esto es otro comentario
+en varias
+lineas
+"""
+
+# mostrar por consola
+
+print("¡Hola Mundo! comillas dobles")
+print('¡Hola mundo! comillas simple')
+print("'¡Hola mundo!'")
+print('"¡Hola mundo!"')
+
+# Tipo de dato
+
+print(type("¡Hola mundo!"))
+
+print(type(-5))
+print(type(3.5))
+print(type(1 + 3j))
+print(type(True))
+print(type(["Plátano", 10, False, 3.14]))
+print(type({
+  'nombre': 'Rodrigo',
+  'apellido': 'Ahumada',
+  'email': 'rodrigo.ahumada.e@gmail.com',
+  'telefono': 971232411,
+  'edad': 41,
+  'estatura': 1.76,
+  'casado': False,
+  'habilidades': ['JS', 'React JS', 'HTML', 'CSS']
+}))
+
+print(type(("Mercurio", "Venus", "Tierra", "Marte", "Júpiter", "Saturno", "Urano", "Neptuno")))
+print(type({3.14, 9.81, 2.7}))
+print({3.14, 9.81, 9.81, 2.7})
---
+++ 01_variables.py
@@ -0,0 +1,76 @@
+# Variables
+dato = ""
+miVariable = "Texto de la variable"
+print(miVariable)
+print(dato)
+
+mi_variable = "otro texto de la variable" # forma correcta
+print(mi_variable)
+
+_if = True
+print(_if)
+
+'''
+como no definir variables
+first-name
+first@name
+first$name
+num-1
+1num
+'''
+
+'''Tipo string'''
+nombre: str = 'Rodrigo'
+apellido = 'Ahumada'
+pais = 'Chile'
+
+'''tipo numericas'''
+edad = 41
+estatura = 1.76
+constante = 3 + 1j
+
+print(nombre)
+
+nombre = 15
+print(nombre)
+nombre = False
+print(nombre)
+
+'''tipo bool'''
+casado = False
+titulado = True
+
+'''tipo lista'''
+habilidades = ['HTML', 'CSS', 'JS', 'React', 'Python']
+print(habilidades[2])
+
+'''tipo tupla'''
+planetas = ("Mercurio", "Venus", "Tierra", "Marte", "Júpiter", "Saturno", "Urano", "Neptuno")
+print(planetas[5])
+
+'''tipo diccionario'''
+informacion_personal = {
+  'nombre':'Rodrigo',
+  'apellido':'Ahumada',
+  'pais':'Chile',
+  'edad':41
+}
+
+print(informacion_personal["edad"])
+
+'''tipo set'''
+conjunto1 = {4, 5, 7, 8}
+conjunto2 = {9, 3, 7, 5}
+print(f"conjunto1: {conjunto1}")
+print(f"conjunto2: {conjunto2}")
+
+print(f"unión: {conjunto1 | conjunto2}")
+
+print(f"intersección: {conjunto1 & conjunto2}")
+
+print(f"diferencia del conjunto1 sobre el conjunto2: {conjunto1 - conjunto2}")
+print(f"diferencia del conjunto2 sobre el conjunto1: {conjunto2 - conjunto1}")
+
+print("Mi nombre es", nombre, apellido, "y mido", estatura, "m.")
+print("Mi nombre es " + nombre + " " + apellido + " y mido " + str(estatura) + "m.")
+print(f"Mi nombre es {nombre} {apellido} y mido {estatura}m.")   #cadena formateada f-string o interpolación de cadena
